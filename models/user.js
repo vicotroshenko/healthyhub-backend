2f48e69ffebd85f5b26565fa685662c1b2f46232
vicotroshenko <118654114+vicotroshenko@users.noreply.github.com>
2023-10-02 11:30:18 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
 	},
 	password: {
 		type: String,
-		required: [true, "Set pawword for userr"]
+		required: [true, "Set pawword for user"]
 	},
 	gender: {
 		type: String,
